8a3095011dd7a2ec0eef757d2b9d85240bd475df
Akhliddin <najmiddinovahliddin04@gmail.com>
2024-12-09 13:28:30 +0500

    Added all functionalities

--- Apps/main/utils.py
+++ Apps/main/utils.py
@@ -7,36 +7,122 @@ from product.models import Product
 from product_material.models import Product_Materials
 from warehouse.models import Warehouse
 
-def get_products_and_materials(ordered_products: List[dict]) -> Tuple[List[dict], List[str]]:
-    """This function checks all the given ordered products are valid and 
-    collects materials that needed to make these products""" 
+import json
 
+def get_products_and_materials(ordered_products: List[dict]) -> Tuple[List[dict], List[str]]:
+    """
+        This function checks all the given ordered products are valid and 
+        collects materials that needed to make these products
+    """ 
+
+    # We need needed_materials for getting only required materials from warehouse
     needed_materials = set()
 
     for product in ordered_products:
+        # Checking product is in valid format
         serializer = ProductQuantitySerializer(data=product)
         serializer.is_valid(raise_exception=True)
 
-        product['product'] = get_object_or_404(Product, name=product['product_name'].lower())
-        product['materials'] = Product_Materials.objects.filter(product=product['product'])
+        # getting materials that we need to make this product
+        product_obj = get_object_or_404(Product, name=product['product_name'].lower())
+        product['materials'] = Product_Materials.objects.filter(product=product_obj)
         
-        needed_materials = needed_materials.union([rel.material.name for rel in product['materials']])
-
-        del product['product_name']
+        # adding materials to needed_materials
+        needed_materials = needed_materials.union([material.material.name for material in product['materials']])
 
     return ordered_products, needed_materials
 
 
 def get_materials(needed_materials: List[str]) -> dict[str, List[dict]]:
+    """
+        This function collects all materials we need and returns it.
+        Return format: 
+        {
+            "ip": "mato": [
+                { "warehouse_id": 2, "remainder": 200.0, "price": 1600 },
+                { "warehouse_id": 1, "remainder": 12.0, "price": 1500 }
+            ],
+            "zamok": [
+                ...
+            ],
+            ...
+        }
+    """
+
+    # res is just dictionary and it contains lists and dictionaries, as value.
+    # That is why it doesn't affect to the database
     res = {}
 
     for needed_material in needed_materials:
+        
         res[needed_material] = []
-        for rel in Warehouse.objects.filter(material__name = needed_material).order_by('-pk'):
+
+        # filtering needed_material from warehouse, ordering it in reverse order 
+        # to maintain queue DS with list(later we will get 
+        # materials from the end in O(1) Time Complexity)
+
+
+        for material in Warehouse.objects.filter(material__name = needed_material).order_by('-pk'):
             res[needed_material].append({
-                'warehouse_id': rel.pk,
-                'remainder': rel.remainder,
-                'price': rel.price
+                'warehouse_id': material.pk,
+                'remainder': material.remainder,
+                'price': material.price
             })
+
+    return res
+
+
+def get_needed_materials(ordered_products: List[dict], materials_in_warehouse: dict[str, List[dict]]) -> dict:
+    res = {
+        'result': []
+    }
+
+    for product in ordered_products:
+        single_res = {
+            'product_name': product['product_name'].capitalize(),
+            'product_qty': product['product_qty'],
+            'product_materials': []
+        }
+
+        # iterating through all materials that we need to make this product
+        for material in product['materials']:
+            # calculating how much material we need from warehouse
+            quantity = product['product_qty'] * material.quantity
+            
+            m_name = material.material.name
+
+            # while quantity is positive and there are materials we need in warehouse
+            while materials_in_warehouse[m_name] and quantity:
+                # getting min between quantity and material quantity in the warehouse
+                mn_qty = min(quantity, materials_in_warehouse[m_name][-1]['remainder'])
+                quantity -= mn_qty
+
+                # subtracting mn_qty from material quantity in warehouse
+                # if it is empty we will delete it later
+                materials_in_warehouse[m_name][-1]['remainder'] -= mn_qty
+
+                # appending information to single_res about materials we are getting from warehouse
+                single_res['product_materials'].append({
+                    'warehouse_id': materials_in_warehouse[m_name][-1]['warehouse_id'],
+                    'material_name': m_name.capitalize(),
+                    'qty': mn_qty,
+                    'price': materials_in_warehouse[m_name][-1]['price']
+                })
+
+                # if materials run out, we will delete this materials box
+                if materials_in_warehouse[m_name][-1]['remainder'] == 0:
+                    materials_in_warehouse[m_name].pop()
+            
+            # If there is some materials we need, we will add it to singe_res with None values
+            if quantity:
+                single_res['product_materials'].append({
+                    'warehouse_id': None,
+                    'material_name': m_name.capitalize(),
+                    'qty': quantity,
+                    'price': None
+                })
+        
+        # adding single result for one product to result
+        res['result'].append(single_res)
 
     return res
--- Apps/main/views.py
+++ Apps/main/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import ValidationError
 from .serializers import RequestSerializer
-from .utils import get_products_and_materials, get_materials
-
+from .utils import get_products_and_materials, get_materials, get_needed_materials
+import json
 
 @api_view(['POST'])
 def GetListView(request, *args, **kwargs):
@@ -27,12 +27,14 @@ def GetListView(request, *args, **kwargs):
 
     """
 
-    # checking request.data is valid
+    # checking request.data is in valid json format like above
     serializer = RequestSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     
     ordered_products, needed_materials = get_products_and_materials(serializer.data['order'])
 
     materials_in_warehouse = get_materials(needed_materials)
+    print(json.dumps(materials_in_warehouse, indent=4))
+    res = get_needed_materials(ordered_products, materials_in_warehouse)
 
-    return Response({'order': "dasd"})
+    return Response(res)
